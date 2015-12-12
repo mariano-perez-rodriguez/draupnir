57239a543f3802395f27f974c02b022b6bdc5d3b
Mariano Pérez Rodríguez <mariano.perez.rodriguez@gmail.com>
2015-12-11 23:41:29 -0300

    Add draupnir logo

--- main.cpp
+++ main.cpp
@@ -2,7 +2,19 @@
 
 using namespace std;
 
+static void showLogo() {
+  cout << "\n"
+          "---------------------------\n"
+          "   /\\                      \n"
+          "   \\/  Draupnir v0.0.1     \n"
+          "   /\\                      \n"
+          "---------------------------\n"
+          "\n";
+}
+
 int main(int argc, char *argv[]) {
+  showLogo();
+
   cout << "Arguments:" << endl;
   for (int i = 0; i < argc; i++) {
     cout << "  " << i << ": " << argv[i] << endl;
